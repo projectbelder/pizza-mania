d6a2169fea619c49afeeee0b8072e7ea39663670
ahadhasanjion <niallhoran3176@gmail.com>
2022-12-19 22:15:03 +0600

    navbar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Jion</h1>
-        </div>
+        <Navbar></Navbar>
     );
 };
 
