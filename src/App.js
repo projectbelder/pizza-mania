387f987d9828bec819ca2dc71eb96c77a360d15e
rajutalukdar1 <lalutaluker@gmail.com>
2022-12-20 00:22:07 +0600

    work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="bg">
-     <Home></Home>
-    <div className="App">
-      <p>Pizza mania</p>
+      <Home></Home>
     </div>
   );
 }
