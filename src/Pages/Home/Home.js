2e323bb68b063b77387e8fa953afb504820c1bb5
ahadhasanjion <niallhoran3176@gmail.com>
2022-12-20 00:08:49 +0600

    menu

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Menu from '../Menu/Menu';
 import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
-        <Navbar></Navbar>
+        <div>
+            <Navbar></Navbar>
+        <Menu></Menu>
+        </div>
     );
 };
 
